2ad565a0ffb0e669f50b5f82669ecc8d7db3984e
Author <43812455+Userrnam@users.noreply.github.com>
2021-01-28 22:22:44 +0500

    grid

--- src/Grid.hpp
+++ src/Grid.hpp
@@ -1,7 +1,11 @@
 #include <SFML/Graphics.hpp>
-
+#include <vector>
 
 struct Grid : public sf::Drawable, public sf::Transformable {
+
+    sf::VertexArray m_vertices;
+
+    void resize(const sf::RenderWindow& window, float spacing);
 
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "Grid.hpp"
+
+
 int main() {
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Window");
+
+    Grid grid;
+
+    float spacing = 50;
+
+    grid.resize(window, spacing);
 
     while (window.isOpen()) {
 
@@ -13,9 +22,18 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+
+            if (event.type == sf::Event::Resized) {
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
+
+                grid.resize(window, spacing);
+            }
         }
 
         window.clear(sf::Color::White);
+
+        window.draw(grid);
 
         window.display();
     }
