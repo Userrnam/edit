03416e2bbaaade141597f5762adfe70e7d768cb1
Author <43812455+Userrnam@users.noreply.github.com>
2021-02-17 16:12:18 +0300

    initial UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-#include "Grid.hpp"
-#include "Editor.hpp"
-
-sf::Vector2u previousWindowSize;
 
 int main() {
-
     sf::RenderWindow window(sf::VideoMode(800, 600), "Window");
-
-    previousWindowSize = window.getSize();
-
-    Editor editor(window);
 
     while (window.isOpen()) {
 
@@ -20,29 +11,10 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-            } else if (event.type == sf::Event::Resized) {
-                editor.handleResize(event.size);
-
-                previousWindowSize = window.getSize();
-                
-            } else if (event.type == sf::Event::MouseMoved) {
-                editor.update();
-            }
-            else if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Equal) {
-                    editor.m_view.zoom(0.9);
-                } else if (event.key.code == sf::Keyboard::G) {
-                    editor.m_view.zoom(1.1);
-                }
             }
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))  editor.move(sf::Vector2f(-1.0f, 0));
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))  editor.move(sf::Vector2f(1.0f, 0));
-
         window.clear(sf::Color::White);
-
-        editor.draw();
 
         window.display();
     }
